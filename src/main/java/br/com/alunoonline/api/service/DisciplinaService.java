15a0625a504cd953430095c3322919f7c47f1507
Jéssica Furtado <jessicafurtado.adm@gmail.com>
2024-03-26 09:46:51 -0300

    CRUD Disciplina

--- src/main/java/br/com/alunoonline/api/service/DisciplinaService.java
+++ src/main/java/br/com/alunoonline/api/service/DisciplinaService.java
@@ -1,5 +1,14 @@
+package br.com.alunoonline.api.service;
 
+import br.com.alunoonline.api.model.Disciplina;
+import br.com.alunoonline.api.repository.DisciplinaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
+import java.util.Optional;
 @Service
 public class DisciplinaService {
 
