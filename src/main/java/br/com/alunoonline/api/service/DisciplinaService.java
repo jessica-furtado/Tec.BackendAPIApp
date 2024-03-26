37814a0b689fb80eaf8b07ecf9c5bccb78417a2b
Jéssica Furtado <jessicafurtado.adm@gmail.com>
2024-03-26 09:00:24 -0300

    CRUD referente ao objeto Aluno.

--- src/main/java/br/com/alunoonline/api/service/DisciplinaService.java
+++ src/main/java/br/com/alunoonline/api/service/DisciplinaService.java
@@ -3,7 +3,12 @@ package br.com.alunoonline.api.service;
 import br.com.alunoonline.api.model.Disciplina;
 import br.com.alunoonline.api.repository.DisciplinaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DisciplinaService {
@@ -14,4 +19,28 @@ public class DisciplinaService {
     public void create(Disciplina disciplina) {
         disciplinaRepository.save(disciplina);
     }
+
+    public List<Disciplina> findALL() {
+        return disciplinaRepository.findAll();
+    }
+    public Optional<Disciplina> findById(Long id) {
+        return disciplinaRepository.findById(id);
+    }
+    public void update(Long id, Disciplina disciplina) {
+        Optional<Disciplina > disciplinaFromDb = findById(id);
+        if (disciplinaFromDb.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Disciplina não encontrada no banco de dados!");
+        }
+
+        Disciplina disciplinaUpdated = disciplinaFromDb.get();
+
+        disciplinaUpdated.setName(disciplina.getName());
+        disciplinaUpdated.setProfessor(disciplina.getProfessor());
+
+        disciplinaRepository.save(disciplinaUpdated);
+    }
+    public void deleteById(Long id) {
+        disciplinaRepository.deleteById(id);
+    }
+
 }
