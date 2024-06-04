6e4d3b8691cfbfdb30b05c217613e5a68636d41d
Jéssica Furtado <jessicafurtado.adm@gmail.com>
2024-06-04 18:26:19 -0300

    Correção do atributo studentFinancial que estava dando erro na execução do código

--- src/main/java/br/com/alunoonline/api/model/Fatura.java
+++ src/main/java/br/com/alunoonline/api/model/Fatura.java
@@ -19,7 +19,7 @@ public class Fatura implements Serializable {
 
     @ManyToOne
     @JoinColumn(name = "student_financial_id")
-    private FinanceiroAluno studentFinancial;
+    private FinanceiroAluno studentFinancial; //correção do atributo
 
     private LocalDateTime dueDate;
 
